c39b540e4204dd1cb2e69086ad5f4524ad7e9a99
builderjer <34875857+builderjer@users.noreply.github.com>
2022-10-22 09:27:31 -0600

    added public server (#2)
    * added public server
    
    * moved to be higher priority

--- ovos_tts_plugin_mimic3_server/__init__.py
+++ ovos_tts_plugin_mimic3_server/__init__.py
@@ -21,6 +21,7 @@ class Mimic3ServerTTSPlugin(TTS):
     """Interface to Mimic3 Server TTS."""
     public_servers = [
         "http://mycroft.blue-systems.com:59125/api/tts",
+        "https://mimic3.ziggyai.online",
         "https://mimic3.jarbasai.online"
     ]
     default_voices = {
