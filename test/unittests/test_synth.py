34dbadb06cb0e221c7e98252321299c752c8e7fe
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-03-13 14:18:02 -0700

    Fix typos in default server URLs (#6)
    Add unit test to ensure at least one public server is live

--- test/unittests/test_synth.py
+++ test/unittests/test_synth.py
@@ -1,11 +1,31 @@
 import unittest
 
+from ovos_plugin_manager.templates.tts import RemoteTTSException
+
 from ovos_tts_plugin_mimic3_server import Mimic3ServerTTSPlugin
 
 mimic = Mimic3ServerTTSPlugin()
 
 
 class TestTTS(unittest.TestCase):
+    def test_public_servers(self):
+        passing_servers = 0
+        for server in mimic.public_servers:
+            path = f"/tmp/{server.split('/')[2].replace('.', '_')}.wav"
+            mimic.url = server
+            try:
+                audio, phonemes = mimic.get_tts("hello", path, lang="en-gb")
+                self.assertEqual(audio, path)
+                with open(audio, 'rb') as f:
+                    audio_content = f.read()
+                    self.assertIn(b'WAVE', audio_content)
+                passing_servers += 1
+            except RemoteTTSException as e:
+                print(f"{server} failing unit tests")
+                print(e)
+        self.assertGreaterEqual(passing_servers, 1,
+                                "No Public Servers Passing Tests!")
+
     def test_default_voice(self):
         path = "/tmp/hello_ap.wav"
         # also testing that en-gb resolves to the internally used (wrong) en-uk
