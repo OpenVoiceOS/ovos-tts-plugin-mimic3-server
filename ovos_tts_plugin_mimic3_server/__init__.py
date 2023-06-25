b96cd920b78edb1c3ff49c807105c9ab86552d62
builderjer <34875857+builderjer@users.noreply.github.com>
2023-06-25 09:13:09 -0600

    Update __init__.py
    removed not working server

--- ovos_tts_plugin_mimic3_server/__init__.py
+++ ovos_tts_plugin_mimic3_server/__init__.py
@@ -21,8 +21,7 @@ class Mimic3ServerTTSPlugin(TTS):
     """Interface to Mimic3 Server TTS."""
     public_servers = [
         "http://mycroft.blue-systems.com:59125/api/tts",
-        "https://mimic3sample.ziggyai.online/api/tts",
-        "https://tts.smartgic.io/mimic3/api/tts"
+        "https://mimic3sample.ziggyai.online/api/tts"
     ]
     default_voices = {
         # TODO add default voice for every lang
